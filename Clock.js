3626bf42a7fbde2ccbac6a944a69abfa79115da1
balram-g <71721726+balram-g@users.noreply.github.com>
2020-10-04 23:45:07 +0530

    Assignment 5
    Digital Clock

---
+++ Clock.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import Switch from "react-switch";
+
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { checked: false };
+    this.showHide = this.showHide.bind(this);
+
+    this.state = {
+      time: new Date()
+    };
+  }
+
+  changeName() {
+    let date = new Date().toLoaleString;
+    date += this.state.show === true ? "hide" : "show";
+    return date;
+  }
+  showHide(checked) {
+    const { show } = this.state;
+
+    this.setState({ show: !show });
+    this.setState({ checked });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1 style={{ backgroundColor: "lightblue" }}>Digital Clock</h1>
+        <h2> {this.state.time.toLocaleTimeString()}</h2>
+        <div className="container">
+          <div>
+            <div>
+              <Switch
+                id="normal-switch"
+                onChange={this.showHide}
+                checked={this.state.checked}
+              />
+              {this.state.show && (
+                <div>
+                  <h2> {this.state.time.toLocaleString()}</h2>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Clock;
